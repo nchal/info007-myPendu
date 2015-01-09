11552b0dc85106b29510f05f0643100744a98cb1
Chisci <Tchisci@laposte.net>
2015-01-09 14:56:32 +0100

    debut logique metier

--- src/main/java/fr/uds/info007/myPendu/controller/MyGame.java
+++ src/main/java/fr/uds/info007/myPendu/controller/MyGame.java
@@ -1,5 +1,19 @@
 package fr.uds.info007.myPendu.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyGame {
+	
+	String motMystere;
+	int nbChance;
+	List<Character> lettersUsed;
+	
+	public MyGame(){
+		nbChance = 0;
+		lettersUsed = new ArrayList<Character>();
+		motMystere = "test";
+	}
+	
 
 }
